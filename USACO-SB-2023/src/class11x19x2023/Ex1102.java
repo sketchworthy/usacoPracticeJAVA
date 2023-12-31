047f4924b5cba66574b543fd90e23e447758336d
sketchworthy <81887088+sketchworthy@users.noreply.github.com>
2023-12-31 14:39:43 -0500

    haybaleHarvest: removed case of bessie going right, then turning around, since redundant

--- USACO-SB-2023/src/class11x19x2023/Ex1102.java
+++ USACO-SB-2023/src/class11x19x2023/Ex1102.java
@@ -10,6 +10,10 @@ package class11x19x2023;
  * far as possible. using this algorithm, we can simulate the n 
  * possible versions of this algorithm by bessie using
  * each of the n haybales as turning points!
+ * another optimization we can make is to consider just bess
+ * going left and turning around. this is because bess going
+ * left then turning right hits all the same spots as bess going
+ * right and turning left.
  * 
  * difficulty: got into a flow state w implementing this problem. 
  * straightforward once u get the idea of simulating for each of 
@@ -31,8 +35,8 @@ public class Ex1102 {
 	static long maxTaste=0; // ans: max total tastiness bess can eat
 	
 	public static void main(String[] args) throws Exception {
-//		BufferedReader in = new BufferedReader(new FileReader("02.in"));
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader in = new BufferedReader(new FileReader("02.in"));
+//		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		n = Integer.parseInt(st.nextToken()); // # of haybales
@@ -76,9 +80,12 @@ public class Ex1102 {
 		else upper = lower+1;
 		
 		// simulate for each of n haybales being the turning point haybale
-		for(int j=0;j<n;j++) { // j: index of haybale bess turns around on
+		for(int j=0;j<=downer;j++) { // j: index of haybale bess turns around on
 			solve(j);
 		}
+		
+		// simulate an extra time for bess going right at the very start
+		// TODO
 		
 		out.print(maxTaste);
 		
@@ -90,30 +97,24 @@ public class Ex1102 {
 		// update maxTaste when index j of haybales[][] is the turning point
 		
 		int higher, lower;
-		if(Math.abs(haybales[j][0]-a)>k) return; 
+		if(a-haybales[j][0]>k) return; 
 		// leave for another iteration if k is not big enough for
-		// bess to go all the way from a to haybales[j][0]
+		// bess to go left from a to haybales[j][0]
 		
 		// now, bess will definitely make it from a to haybales[j][0].
-		// how much tastiness gathered along the way?
+		
+		// find tastiness gathered along the way
 		int currTaste=0;
-		if(haybales[j][0]<a) { // left of a
-			if(j==0) currTaste = ps[downer];
-			else currTaste = ps[downer]-ps[j-1];
-		}
-		else { // on a, or to the right
-			if(haybales[0][0]==a) currTaste = ps[j];
-			else currTaste = ps[j]-ps[upper-1];
-		}
+		if(j==0) currTaste = ps[downer];
+		else currTaste = ps[downer]-ps[j-1];
 		
 		
-		int lenLeft = k-2*Math.abs(haybales[j][0]-a); 
-		// how many steps bess has left after going from position a 
+		int lenLeft = k-2*(a-haybales[j][0]); 
+		// how many steps left after going left from position a 
 		// to haybales[j][0] and back
 		if(lenLeft>0) { // if bess has enough juice left to
-			// go from position a in opposite direction to 
-			// grab more cows
-			if(haybales[j][0]<a && haybales[upper][0]-a<=lenLeft) {
+			// go from position a right to grab more cows
+			if(haybales[upper][0]-a<=lenLeft) {
 				// turning point was left of a and we can get to more haybales
 				// binary search for largest haybale index
 				// bess can make it to
@@ -136,32 +137,6 @@ public class Ex1102 {
 
 				// use ps array to update currTaste
 				currTaste+=ps[higher]-ps[upper-1];
-				
-			}
-			else if (a-haybales[downer][0]<=lenLeft){ 
-				// turning point to the right of a and can make it
-				// binary search for smallest haybale index
-				// bess can make it to
-				higher = downer;
-				lower = 0;
-				while(lower<higher) {
-					int mid = (higher+lower)/2;
-					
-					if(a-haybales[mid][0]<lenLeft) {
-						higher = mid;
-					}
-					else if(a-haybales[mid][0]>lenLeft) {
-						lower = mid+1;
-					}
-					else {
-						higher=mid;
-						lower=mid;
-					}
-				}
-
-				// use ps array to update currTaste
-				if(higher==0) currTaste+=ps[lower];
-				else currTaste+=ps[lower]-ps[higher-1];
 				
 			}
 		}
