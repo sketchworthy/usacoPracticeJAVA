6b01b03f4157aa7ad3c029f58612c7a785156611
sketchworthy <81887088+sketchworthy@users.noreply.github.com>
2024-01-01 01:30:09 -0500

    millionaireMadness: implemented inverse binary search solution that uses dfs to find clusters

--- USACO-SB-2023/src/class11x12x2023/HW1001.java
+++ USACO-SB-2023/src/class11x12x2023/HW1001.java
@@ -5,15 +5,19 @@ package class11x12x2023;
  * going from height a to b where a<b requires a ladder length of
  * at least b-a.
  * 
- * idea: notice since we're looking for ladder height, this is an inverse
- * binary search problem.
- * you can perform dfs to find all possible paths from
- * top left corner to bottom right corner by just keeping a visited arr
+ * idea: i was originally stuck on this problem bc i thought i needed
+ * to use dfs straight up, but got stuck on how to keep a separate
+ * visited arr for every function call. however that is actually not the way.
+ * notice since we're looking for ladder height, this is an inverse
+ * binary search problem. then, for every possible ladder, we can 
+ * perform dfs to find the cluster of all paths reachable from the
+ * top left corner and keep a visited arr. if bottom right corner
+ * reachable, this ladder len works.
  * 
  * difficulty: i wanted to make it so that every dfs call
  * has its own copy of a visited arr so it can track its own
  * visits but that didnt rlly work :(
- * instead, 
+ * instead, inverse binary search ftw! TODO
  * 
  */
 import java.util.*;
@@ -24,7 +28,8 @@ public class HW1001 {
 	static ArrayList<ArrayList<Integer>> adj; // every node is adj to 4 neighbors
 	// node [x][y] is ID x*m+y, which covers 0 thru m*n-1
 	static int[][] grid;
-	static int[][] ans; // ans[x][y] min height of ladder that can get to that point on grid
+	static int ladderLen;
+	static boolean[][] visited;
 	
 	static int[] dr = {1,0,-1,0};
 	static int[] dc = {0,1,0,-1};
@@ -42,13 +47,15 @@ public class HW1001 {
 			}
 		}
 		
-		// fill adj
+		// fill adj and find tallest height of coin pile
 		adj = new ArrayList<>();
 		for(int j=0;j<m*n;j++) {
 			adj.add(new ArrayList<>());
 		}
+		int tallestCoinHeight=0;
 		for(int j=0;j<m;j++) {
 			for(int k=0;k<n;k++) { // for node[j][k]
+				tallestCoinHeight = Math.max(tallestCoinHeight, grid[j][k]);
 				int currI = j*m+k;
 				for(int i=0;i<4;i++) {
 					if(j+dr[i]>=0 && j+dr[i]<m && k+dc[i]>=0 && k+dc[i]<n) {
@@ -58,48 +65,37 @@ public class HW1001 {
 			}
 		}
 		
-		// set up ans
-		ans = new int[m][n];
-		for(int j=0;j<m;j++) Arrays.fill(ans[j], Integer.MAX_VALUE);
-		ans[0][0]=0;
+		// inverse binary search for smallest ladder length that will work
+		int low = 0;
+		int high = tallestCoinHeight;
+		while(low<high) {
+			ladderLen = (low+high)/2;
+			
+			visited = new boolean[m][n];
+			
+			// find cluster of all visit-able squares with dfs
+			dfs(0);
+			
+			if(visited[m-1][n-1]) {
+				high = ladderLen;
+			}
+			else low = ladderLen+1;
+		}
 		
-		boolean[][] visited = new boolean[m][n];
-		visited[0][0]=true;
-		
-		// dfs
-		dfs(0,visited);
-		
-		out.println(ans[m-1][n-1]);
+		out.print(low);
 
 		in.close();
 		out.close();
 	}
 	
-	static void dfs(int node, boolean[][] visitd) { 
-		if(node==m*n-1 || visitd[m-1][n-1]==true) return;
+	static void dfs(int node) {
+		// find cluster of all visit-able squares with ladderLen 
+		visited[node/m][node%m]=true;
 		
-		// make a copy of bool array to pass on to future dfs calls
-		boolean[][] visited = new boolean[m][n]; 
-		for(int j=0;j<m;j++) {
-			for(int k=0;k<n;k++) {
-				visited[j][k]=visitd[j][k];
-			}
-		}
-		
-		// visited nodes so far, and prev node
 		for(int nbor:adj.get(node)) {
-			// visit nbor from node and update ans
-			if(!visited[nbor/m][nbor%m]) {
-				visited[nbor/m][nbor%m]=true;
-				if(grid[nbor/m][nbor%m]<=grid[node/m][node%m]) { // if they fall to get to nbor
-					ans[nbor/m][nbor%m]=Math.min(ans[node/m][node%m], ans[nbor/m][nbor%m]);
-				}
-				else {
-					ans[nbor/m][nbor%m]=Math.min( Math.max(ans[node/m][node%m], 
-						grid[nbor/m][nbor%m]-grid[node/m][node%m]),
-						 ans[nbor/m][nbor%m]); // min of factoring in ladder len vs whats already there
-				}
-				dfs(nbor,visited);
+			if(visited[nbor/m][nbor%m])continue;
+			if(grid[nbor/m][nbor%m]<=ladderLen+grid[node/m][node%m]) {
+				dfs(nbor);
 			}
 		}
 	}
