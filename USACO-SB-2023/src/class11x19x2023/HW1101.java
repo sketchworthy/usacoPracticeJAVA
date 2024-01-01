e1ca1909b68117f315be8f52437979f0c9b52509
sketchworthy <81887088+sketchworthy@users.noreply.github.com>
2024-01-01 01:11:36 -0500

    haybaleHarvest: figured out bessie just going left dont work so reverting last commit, fixed a bug in binary search. sumDivByP: finished implementation draft 1

--- USACO-SB-2023/src/class11x19x2023/HW1101.java
+++ USACO-SB-2023/src/class11x19x2023/HW1101.java
@@ -1,12 +1,20 @@
 package class11x19x2023;
-/* Sum divisible by P _status{COMPLETE or not}_
+/* Sum divisible by P COMPLETE, TO REVIEW & SUBMIT TO CANVAS
  * Given an array A of pos ints, remove shortest contiguous subarr such that 
  * remaining total sum is 0 mod p. Report the length of the subarr u remove.
  * If you must remove the whole arr, return -1
  * 
- * idea: prefix sum arrs, 
+ * idea: keep a prefix sum (mod p) array of A. Record m, the mod p of the 
+ * total sum of array A. We basically want to find the shortest difference 
+ * in indices that sums to m.
+ * We meanwhile keep a hash map. keys: mod p values of ps array sums. 
+ * Values: last index that that the key showed up in ps sum arr.
+ * To do this, we iterate thru the ps arr once. At every index j we get 
+ * ps[j], the sum up to that point. We then check the hash map val for 
+ * ps[j]-m mod p. If it exists we update the ans to min of ans and 
+ * j-(index by hash map adjusted with +1 mb)
  * 
- * difficulty:
+ * difficulty: ez. straightforward ps.
  * 
  */
 import java.util.*;
@@ -23,37 +31,30 @@ public class HW1101 {
 		st = new StringTokenizer(in.readLine());
 		for(int j=0;j<n;j++) {
 			A[j]=Integer.parseInt(st.nextToken())%p;
-		}
+		}	
 		
-		// create freq ps arr
-		ArrayList<ArrayList<Integer>> modPindices = new ArrayList<>();
-		// each of 0,...i...p-1 lists will give the indices x where ps[x]=i
-		for(int j=0;j<p;j++) {
-			modPindices.add(new ArrayList<Integer>());
-		}
-		// add -1 to modPindices.get(0)
-		modPindices.get(0).add(-1);
-		
-		// create ps arr
+		// create ps mod p arr
 		int[] ps = new int[n];
 		ps[0]=A[0];
 		for(int j=1;j<n;j++) {
 			ps[j]=(ps[j-1]+A[j])%p; // mod p
-			modPindices.get(ps[j]).add(j);
 		}
+
+		int ans = Integer.MAX_VALUE;
+		int m = ps[n-1]; // mod p of total sum of array
+		HashMap<Integer,Integer> indicesOfPs = new HashMap<>();
+		// indicesOfPs[x] gives most recent index of ps[] that equalled x
 		
-		// find min diff in identical mods using 
-		int min = Integer.MAX_VALUE;
-		for(int j=0;j<p;j++) {
-			for(int k=modPindices.get(j).size()-1;k>0;k--) {
-				int debg=modPindices.get(j).get(k)-modPindices.get(j).get(k-1);
-				min=Math.min(min, modPindices.get(j).get(k)-modPindices.get(j).get(k-1));
+		// iterate thru ps once, updating hashmap and ans
+		for(int j=0;j<n;j++) {
+			indicesOfPs.put(ps[j], j);
+			if(indicesOfPs.containsKey((ps[j]-m)%p)) {
+				ans = Math.min(ans, j-indicesOfPs.get((ps[j]-m)%p));
 			}
 		}
 		
-		
-		if(min==Integer.MAX_VALUE)out.print(-1);
-		else out.print(min);
+		if(ans==Integer.MAX_VALUE)out.print(-1);
+		else out.print(ans);
 		
 		in.close();
 		out.close();
