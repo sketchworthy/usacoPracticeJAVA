a52fd0fccf9337ea8476091a2ffc4eb750f72531
sketchworthy <81887088+sketchworthy@users.noreply.github.com>
2024-01-05 20:12:12 -0500

    changed ints to longs

--- USACO-SB-2023/src/class11x19x2023/HW1101.java
+++ USACO-SB-2023/src/class11x19x2023/HW1101.java
@@ -1,6 +1,6 @@
 package class11x19x2023;
 /* Sum divisible by P COMPLETE, TO REVIEW & SUBMIT TO CANVAS
- * Given an array A of pos ints, remove shortest contiguous subarr such that 
+ * Given an array A of pos longs, remove shortest contiguous subarr such that 
  * remaining total sum is 0 mod p. Report the length of the subarr u remove.
  * If you must remove the whole arr, return -1
  * 
@@ -14,7 +14,7 @@ package class11x19x2023;
  * ps[j]-m mod p. If it exists we update the ans to min of ans and 
  * j-(index by hash map adjusted with +1 mb)
  * 
- * difficulty: ez. straightforward ps.
+ * difficulty: ez. straightforward ps. some debug trouble as usual tho.
  * 
  */
 import java.util.*;
@@ -22,32 +22,32 @@ import java.io.*;
 
 public class HW1101 {
 	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new FileReader("03.in"));
-//		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedReader in = new BufferedReader(new FileReader("03.in"));
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		int p = Integer.parseInt(st.nextToken());
-		int[] A = new int[n]; // mod p
+		long p = Long.parseLong(st.nextToken());
+		long[] A = new long[n]; // mod p
 		st = new StringTokenizer(in.readLine());
 		for(int j=0;j<n;j++) {
-			A[j]=Integer.parseInt(st.nextToken())%p;
+			A[j]=Long.parseLong(st.nextToken())%p;
 		}	
 		
 		// create ps mod p arr
-		int[] ps = new int[n];
+		long[] ps = new long[n];
 		ps[0]=A[0];
 		for(int j=1;j<n;j++) {
 			ps[j]=(ps[j-1]+A[j])%p; // mod p
 		}
 
-		int ans = Integer.MAX_VALUE;
-		int m = ps[n-1]; // mod p of total sum of array
-		HashMap<Integer,Integer> indicesOfPs = new HashMap<>();
+		long ans = Long.MAX_VALUE;
+		long m = ps[n-1]; // mod p of total sum of array
+		HashMap<Long,Integer> indicesOfPs = new HashMap<>();
 		// indicesOfPs[x] gives most recent index of ps[] that equalled x
 		
 		// iterate thru ps once, updating hashmap and ans
-		indicesOfPs.put(0,-1);
+		indicesOfPs.put((long)0,-1);
 		for(int j=0;j<n;j++) {
 			indicesOfPs.put(ps[j], j);
 			if(indicesOfPs.containsKey((ps[j]-m)%p)) {
@@ -55,7 +55,7 @@ public class HW1101 {
 			}
 		}
 		
-		if(ans==Integer.MAX_VALUE || ans==n)out.print(-1);
+		if(ans==Long.MAX_VALUE || ans==n)out.print(-1);
 		else out.print(ans);
 		
 		in.close();
