daed26d7b768208dbde02ed8710693182f9e8260
sketchworthy <81887088+sketchworthy@users.noreply.github.com>
2024-01-13 12:36:29 -0500

    attempt at implementing USACO Dec 2023 q2

--- USACODec2023/src/silver2sol.java
+++ USACODec2023/src/silver2sol.java
@@ -35,9 +35,60 @@ public class silver2sol {
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		int n = Integer.parseInt(st.nextToken()); // # of barns/nodes
 		int k = Integer.parseInt(st.nextToken()); // # of barns in cycle
+		int[] A = new int[n]; // A[x] gives index node x is at in A's cycle
+		int[] B = new int[n]; // B[x]: index node x is at in B's cycle
 		
+		boolean[] inACycle = new boolean[n];
+		int cntOfDistinctNodesInCycles=0;
 		
-		out.println(n);
+		HashMap<Integer, Integer> freq = new HashMap<>(); 
+		// keeps track of frequency of differences between the same node #
+		HashMap<Integer, Integer> freq2 = new HashMap<>();
+		// for comparing A against flipped B
+		
+		Arrays.fill(A, -1);
+		Arrays.fill(B, -1);
+		st = new StringTokenizer(in.readLine());
+		for(int j=0;j<k;j++) {
+			int temp = Integer.parseInt(st.nextToken())-1;
+			if(!inACycle[temp]) {
+				cntOfDistinctNodesInCycles++;
+				inACycle[temp]=true;
+			}
+			A[temp] = j;
+		}
+		st = new StringTokenizer(in.readLine());
+		for(int j=0;j<k;j++) {
+			int temp = Integer.parseInt(st.nextToken())-1;
+			if(!inACycle[temp]) {
+				cntOfDistinctNodesInCycles++;
+				inACycle[temp]=true;
+			}
+			B[temp] = j;
+		}
+		
+		for(int j=0;j<k;j++) { // for node
+			if(A[j]==-1 || B[j]==-1) continue;
+			int diff;
+//			diff = (B[j]-A[j]+k)%k; // ensures >0
+//			freq.put(diff,freq.getOrDefault(diff, 0)+1);
+//
+			diff = ((k-A[j]-B[j])%k+k)%k; // opposite way
+			freq2.put(diff,freq2.getOrDefault(diff, 0)+1);
+		}
+		
+		int ans = 0;
+		for(Integer key : freq.keySet()) {
+			ans = Math.max(ans, freq.get(key));
+		}
+		for(Integer key : freq2.keySet()) {
+			ans = Math.max(ans, freq2.get(key));
+		}
+
+
+		out.println(ans +n-cntOfDistinctNodesInCycles); 
+		// max # of nodes that can stay the same in cycles
+		// + # of nodes never mentioned in any cycle
 		
 		in.close();
 		out.close();
