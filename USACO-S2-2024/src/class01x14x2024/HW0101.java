8b3ee25b4c5573db64a4ae1b348a54d811bc3a8f
sketchworthy <81887088+sketchworthy@users.noreply.github.com>
2024-01-14 14:33:43 -0500

    setting up new class folder

---
+++ USACO-S2-2024/src/class01x14x2024/HW0101.java
@@ -0,0 +1,31 @@
+package class01x14x2024;
+/* USACO [year] [month] Silver _status{COMPLETE or not}_
+ * [problem desc/link]
+ * 
+ * idea: 
+ * 
+ * implementation: 
+ * 
+ * difficulty:
+ * 
+ */
+import java.util.*;
+import java.io.*;
+
+public class HW0101 {
+	public static int w = 1;
+
+	public static void main(String[] args) throws Exception {
+		// take input
+		//		BufferedReader in = new BufferedReader(new FileReader("0"+w+".in"));
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken()); //
+
+		out.println();
+
+		in.close();
+		out.close();
+	}
+}
